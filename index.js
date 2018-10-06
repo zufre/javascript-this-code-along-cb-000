9f951fe428f3f61162e271f265495da983f45fc6
Cedric David <cedric.david@btopenworld.com>
2018-10-06 14:31:49 +0000

    Done.

--- index.js
+++ index.js
@@ -4,3 +4,20 @@ var pbj = {
   ingredients: ["peanut butter", "jelly"],
   cut: "triangles"
 }
+function Sandwich(bread, ingredients, name) {
+  this.bread = bread;
+  this.ingredients = ingredients;
+  this.name = name;
+  this.describe = function() {
+    console.log("Your " + this.name + " includes: " + this.ingredients.join(", ") + ". Yum!");
+  }
+}
+
+var pbj = new Sandwich("wheat", ["chunky peanut butter", "blackberry preserves"], "PB&Jam");
+
+var salad = {
+  ingredients: ["croutons", "romaine hearts", "steak", "parmesan", "caesar dressing"],
+  name: "Steak Caesar"
+}
+
+salad.describe = pbj.describe.bind(salad);
